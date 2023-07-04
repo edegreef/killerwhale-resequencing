34fc868bb26372fc3a26c3ce3fabda162cdfa72e
Evelien de Greef <49288304+edegreef@users.noreply.github.com>
2023-07-04 13:50:58 -0500

    Update and rename strataG_prep.sh to 08-strataG_prep.sh

--- project-demography/08-strataG_prep.sh
+++ project-demography/08-strataG_prep.sh
@@ -6,7 +6,6 @@
 
 # Separate by pops
 vcftools --gzvcf orca_snps_q30_biallelic_HWE0.005_miss0.4.vcf.gz --keep high_arc_P1 --recode --recode-INFO-all --out orca_snps_q30_biallelic_HWE0.005_miss0.4.P1
-
 vcftools --gzvcf orca_snps_q30_biallelic_HWE0.005_miss0.4.vcf.gz --keep mid_arc_P2 --recode --recode-INFO-all --out orca_snps_q30_biallelic_HWE0.005_miss0.4.P2
 
 mv orca_snps_q30_biallelic_HWE0.005_miss0.4.P1.recode.vcf orca_snps_q30_biallelic_HWE0.005_miss0.4.P1.vcf
@@ -14,7 +13,6 @@ mv orca_snps_q30_biallelic_HWE0.005_miss0.4.P2.recode.vcf orca_snps_q30_bialleli
 
 # Remove snps with missing data and also include maf filter
 vcftools --vcf orca_snps_q30_biallelic_HWE0.005_miss0.4.P1.vcf --max-missing 1.0 --maf 0.05 --recode --recode-INFO-all --out orca_snps_q30_biallelic_HWE0.005.P1.nomiss.maf0.05
-
 vcftools --vcf orca_snps_q30_biallelic_HWE0.005_miss0.4.P2.vcf --max-missing 1.0 --maf 0.05 --recode --recode-INFO-all --out orca_snps_q30_biallelic_HWE0.005.P2.nomiss.maf0.05
 
 mv orca_snps_q30_biallelic_HWE0.005.P1.nomiss.maf0.05.recode.vcf orca_snps_q30_biallelic_HWE0.005.P1.nomiss.maf0.05.vcf
@@ -22,12 +20,10 @@ mv orca_snps_q30_biallelic_HWE0.005.P2.nomiss.maf0.05.recode.vcf orca_snps_q30_b
 
 # select random 25 K snps - run this step for different sets (sets 1-3 and for each pop)
 /home/degreefe/programs/plink --thin-count 25000 --allow-extra-chr --make-bed --vcf orca_snps_q30_biallelic_HWE0.005.P1.nomiss.maf0.05.vcf --set-missing-var-ids @:#\$1,\$2 --out orca_snps_q30_biallelic_HWE0.005.P1.nomiss.maf0.05.thinned25K.set1 --double-id
-
 /home/degreefe/programs/plink --thin-count 25000 --allow-extra-chr --make-bed --vcf orca_snps_q30_biallelic_HWE0.005.P2.nomiss.maf0.05.vcf --set-missing-var-ids @:#\$1,\$2 --out orca_snps_q30_biallelic_HWE0.005.P2.nomiss.maf0.05.thinned25K.set1 --double-id
 
 # convert bfile back to vcf
 /home/degreefe/programs/plink --bfile orca_snps_q30_biallelic_HWE0.005.P1.nomiss.maf0.05.thinned25K.set1 --recode vcf --out orca_snps_q30_biallelic_HWE0.005.P1.nomiss.maf0.05.thinned25K.set1 --allow-extra-chr
-
 /home/degreefe/programs/plink --bfile orca_snps_q30_biallelic_HWE0.005.P2.nomiss.maf0.05.thinned25K.set1 --recode vcf --out orca_snps_q30_biallelic_HWE0.005.P2.nomiss.maf0.05.thinned25K.set1 --allow-extra-chr
 
 # use the vcf in R to run strataG
